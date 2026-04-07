e81537cb28e484716240fc883b2c231c3d43ae96
shehabt1000-boop <shehabt1000@gmail.com>
2026-04-07 04:17:39 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,44 +1,28 @@
 const CACHE_NAME = 'arrahman-v1';
-
 const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png',
-  './screenshot-mobile.png',
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/screenshot-mobile.png',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
 ];
 
-// تثبيت الـ Service Worker
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
-// تفعيل النسخة الجديدة وحذف القديمة
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null))
+    ).then(() => self.clients.claim())
   );
 });
 
-// جلب البيانات: كاش أولاً
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+  if (event.request.url.includes('firestore') || event.request.url.includes('google')) return;
+  event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
 });
