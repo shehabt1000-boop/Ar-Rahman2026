5be9193863fa7fb2255c7590fc45d1b6fc5e478f
شهاب طارق <shehabt1000@gmail.com>
2026-07-04 13:26:43 +0300

    Add Firebase integration

--- api/send-notification.js
+++ api/send-notification.js
@@ -1,7 +1,29 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    success: true,
-    message: "API works successfully",
-    time: new Date().toISOString()
-  });
-} 
+import { db } from "../lib/firebase.js";
+
+export default async function handler(req, res) {
+  try {
+    const snapshot = await db.collection("users_tokens").get();
+
+    const tokens = [];
+
+    snapshot.forEach((doc) => {
+      const data = doc.data();
+
+      if (data.token) {
+        tokens.push(data.token);
+      }
+    });
+
+    return res.status(200).json({
+      success: true,
+      totalTokens: tokens.length,
+      tokens
+    });
+
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: error.message
+    });
+  }
+}
